bf6df809ae83e11ff7acdf5ca4b22d46d9862372
Badru Olumide David <badrudavid2014@gmail.com>
2023-09-15 03:51:13 -0700

    MovieBox

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,14 @@
 import './Card.css'
 export default function Card() {
-const movieList = [
-    {title: 'Legend of the Seeker Complete Season', poster: 'https://media.istockphoto.com/id/512574420/photo/lamp-of-wishes-in-the-desert.webp?b=1&s=170667a&w=0&k=20&c=SVoUtxrKMAurYrxNJhVqt2eHsRrgREx_FQ1pljoxFeY=', year: '2010' }]
-    const title = movieList[0].title;
-    const poster = movieList[0].poster;
-    const year = movieList[0].year;
+    const movieList = []
+    //const title = movieList;
+    //const poster = movieList;
+    //const year = movieList;
     return (
-        <div className="card" data-testid: movie-card>
-            <img src={poster} alt="movie" data-testid: movie-poster/>
-             <h2 data-testid: movie-title>{title}</h2>
-            <h4 data-testid: movie-release-date>Released: {year}</h4>
+        <div className="card" data-testid='movie-card'>
+            <img src="" alt="movie" data-testid='movie-poster' />
+            <h2 data-testid='movie-title'>Title</h2>
+            <h4 data-testid='movie-release-date'>Released: </h4>
         </div>
     )
 }
